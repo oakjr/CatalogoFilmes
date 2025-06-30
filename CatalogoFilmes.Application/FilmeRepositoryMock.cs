76fbb6d257b9f5374a9d9e9cbfb591cbc1390685
Omar.Kattwinkel <omar.kattwinkel@gmail.com>
2025-06-30 11:54:17 -0300

    'more films'

--- CatalogoFilmes.Application/FilmeRepositoryMock.cs
+++ CatalogoFilmes.Application/FilmeRepositoryMock.cs
@@ -8,7 +8,15 @@ namespace CatalogoFilmes.Application.Repositories
         private readonly List<Filme> _filmes = new()
         {
             new Filme { Titulo = "Matrix", Diretor = "Wachowski", Ano = 1999, Genero = "Ficção Científica" },
-            new Filme { Titulo = "O Poderoso Chefão", Diretor = "Francis Ford Coppola", Ano = 1972, Genero = "Drama" }
+            new Filme { Titulo = "O Poderoso Chefão", Diretor = "Francis Ford Coppola", Ano = 1972, Genero = "Drama" },
+            new Filme { Titulo = "Interestelar", Diretor = "Christopher Nolan", Ano = 2014, Genero = "Ficção Científica" },
+            new Filme { Titulo = "Cidade de Deus", Diretor = "Fernando Meirelles", Ano = 2002, Genero = "Drama" },
+            new Filme { Titulo = "Parasita", Diretor = "Bong Joon-ho", Ano = 2019, Genero = "Suspense" },
+            new Filme { Titulo = "Forrest Gump", Diretor = "Robert Zemeckis", Ano = 1994, Genero = "Drama" },
+            new Filme { Titulo = "Vingadores: Ultimato", Diretor = "Anthony Russo, Joe Russo", Ano = 2019, Genero = "Ação" },
+            new Filme { Titulo = "A Origem", Diretor = "Christopher Nolan", Ano = 2010, Genero = "Ficção Científica" },
+            new Filme { Titulo = "O Senhor dos Anéis: O Retorno do Rei", Diretor = "Peter Jackson", Ano = 2003, Genero = "Fantasia" },
+            new Filme { Titulo = "Clube da Luta", Diretor = "David Fincher", Ano = 1999, Genero = "Drama" }
         };
 
         public IEnumerable<Filme> ObterTodos() => _filmes;
